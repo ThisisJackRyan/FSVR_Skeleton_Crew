15525d4136a9166ad178c21be12329937c12150a
FLVR1001\fsvr1001 <plex>
2018-07-27 16:51:35 -0500

    cannon work, all but proper rotations

--- FlipSwitch VR - Skeleton Crew/Assets/_Scripts/Player/CannonInteraction.cs
+++ FlipSwitch VR - Skeleton Crew/Assets/_Scripts/Player/CannonInteraction.cs
@@ -1,36 +1,37 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
 public class CannonInteraction : NetworkBehaviour {
 
-	public void Fire(GameObject cannon) {
-		if (!isServer)
+	public void Fire( GameObject cannon ) {
+		if ( !isServer )
 			return;
 
-		FireCannon(cannon);
+		FireCannon( cannon );
 	}
 
-	private void FireCannon(GameObject cannon) {
+	private void FireCannon( GameObject cannon ) {
 		cannon.GetComponent<Cannon>().CreateCannonBall();
 		Captain.playersFiredCannons[this] = true;
 		Captain.instance.CheckPlayersCannonFiring();
 
-		RpcFireCannon(cannon);
+		RpcFireCannon( cannon );
 	}
 
 	[ClientRpc]
-	private void RpcFireCannon(GameObject cannon) {
-		if (isServer)
+	private void RpcFireCannon( GameObject cannon ) {
+		if ( isServer )
 			return;
 		cannon.GetComponent<Cannon>().CreateCannonBall();
 	}
 
 	private void Start() {
-		if (isServer) {
-			print(name + " enabled server check");
-			Captain.playersFiredCannons.Add(this, false);
+		if ( isServer ) {
+			print( name + " enabled server check" );
+			Captain.playersFiredCannons.Add( this, false );
 		}
 
 		mastInteraction = GetComponent<MastInteraction>();
@@ -43,58 +44,92 @@ public class CannonInteraction : NetworkBehaviour {
 	MastInteraction mastInteraction;
 	Cannon cannonCurrentlyAiming;
 	int indexOfClosest = -1;
+	bool leftHandInteracting, rightHandInteracting;
 
 	void Update() {
 		if ( !isLocalPlayer ) {
 			return;
 		}
 
-		//closest hasnt been found, grabbing is allowed
-		if ( indexOfClosest == -1 && mastInteraction.emptyLeftHand && Controller.LeftController.GetPressDown(Controller.Grip)) {
+		//closest hasnt been found, grabbing is allowed  //are we changing the -1 sentinel?
+		if ( !leftHandInteracting && mastInteraction.emptyLeftHand && Controller.LeftController.GetPressDown( Controller.Grip ) ) {
+			CmdHandleAiming( true );
 			//print("inside button down left");
+		}
+
+		if (!rightHandInteracting && mastInteraction.emptyRightHand && Controller.RightController.GetPressDown( Controller.Grip ) ) {
+			CmdHandleAiming( false );
+		}
+
+		//player has grabbed wheel
+		if (leftHandInteracting && Controller.LeftController.GetPress( Controller.Grip ) ) {
+			if ( Vector3.Distance( mastInteraction.leftHand.position, cannonCurrentlyAiming.aimingNodes[indexOfClosest].position ) > maxReachToCannonWheel ) {
+				leftHandInteracting = false;
+				CmdStopInteracting(true);
+			}
+		}
+
+		if (rightHandInteracting && Controller.RightController.GetPress( Controller.Grip ) ) {
+			if ( Vector3.Distance( mastInteraction.rightHand.position, cannonCurrentlyAiming.aimingNodes[indexOfClosest].position ) > maxReachToCannonWheel ) {
+				rightHandInteracting = false;
+				CmdStopInteracting(false);
+			}
+		}
+
+		//player let go
+		if ( leftHandInteracting && Controller.LeftController.GetPressUp( Controller.Grip ) ) {
+			//print( "inside up left" );
+			leftHandInteracting = false;
+			CmdStopInteracting(true);
+		}
+
+		if ( rightHandInteracting && Controller.RightController.GetPressUp( Controller.Grip ) ) {
+			//print( "inside up right" );
+			rightHandInteracting = false;
+			CmdStopInteracting(false);
+		}
+	}
+
+	[Command]
+	private void CmdHandleAiming( bool isLeft ) {
+		if ( isLeft ) {
 			Collider[] hits = Physics.OverlapSphere( mastInteraction.leftHand.position, mastInteraction.radius );
-			for (int i = 0; i < hits.Length; i++) {
-				if (hits[i].transform.tag == "CannonAimingWheel") {
+			for ( int i = 0; i < hits.Length; i++ ) {
+				if ( hits[i].transform.tag == "CannonAimingWheel" ) {
 					cannonCurrentlyAiming = hits[i].GetComponentInParent<Cannon>();
 					//get closest node
 					Transform closest = null;
-					for (int index = 0; index < cannonCurrentlyAiming.aimingNodes.Length; index++) {
+					for ( int index = 0; index < cannonCurrentlyAiming.aimingNodes.Length; index++ ) {
 						Transform node = cannonCurrentlyAiming.aimingNodes[index];
-						node.GetComponent<Renderer>().enabled = true;
 						node.GetComponent<CannonAimNode>().player = this;
-
-						if ( !closest) {
+						if ( !closest ) {
 							closest = node;
 							indexOfClosest = index;
 						} else {
-							if (Mathf.Abs(Vector3.Distance(mastInteraction.leftHand.position, hits[i].transform.position)) <
-							    Mathf.Abs(Vector3.Distance(mastInteraction.leftHand.position, closest.transform.position))) {
+							if ( Mathf.Abs( Vector3.Distance( mastInteraction.leftHand.position, hits[i].transform.position ) ) <
+								Mathf.Abs( Vector3.Distance( mastInteraction.leftHand.position, closest.transform.position ) ) ) {
 								closest = node;
 								indexOfClosest = index;
 							}
 						}
 					}
 
+					RpcStartInteractingOnClient( cannonCurrentlyAiming.gameObject, gameObject, isLeft, indexOfClosest );
 					//got closest node
 					cannonCurrentlyAiming.indexOfFirstGrabbed = indexOfClosest;
-					mastInteraction.leftHandInteracting = true;
+					leftHandInteracting = true;
 				}
 			}
-		}
-
-		if ( indexOfClosest == -1 && mastInteraction.emptyRightHand && Controller.RightController.GetPressDown(Controller.Grip)) {
-			//print( "inside button down right" );
-
+		} else {
 			Collider[] hits = Physics.OverlapSphere( mastInteraction.rightHand.position, mastInteraction.radius );
-			for ( int i = 0; i < hits.Length; i++) {
-				if (hits[i].transform.tag == "CannonAimingWheel" ) {
+			for ( int i = 0; i < hits.Length; i++ ) {
+				if ( hits[i].transform.tag == "CannonAimingWheel" ) {
 					cannonCurrentlyAiming = hits[i].GetComponentInParent<Cannon>();
 					Transform closest = null;
 					for ( int index = 0; index < cannonCurrentlyAiming.aimingNodes.Length; index++ ) {
 						Transform node = cannonCurrentlyAiming.aimingNodes[index];
-						node.GetComponent<Renderer>().enabled = true;
+						//node.GetComponent<Renderer>().enabled = true;
 						node.GetComponent<CannonAimNode>().player = this;
-
 						if ( !closest ) {
 							closest = node;
 							indexOfClosest = index;
@@ -107,64 +142,63 @@ public class CannonInteraction : NetworkBehaviour {
 						}
 					}
 
+					RpcStartInteractingOnClient( cannonCurrentlyAiming.gameObject, gameObject, isLeft, indexOfClosest );
 					cannonCurrentlyAiming.indexOfFirstGrabbed = indexOfClosest;
 
-					mastInteraction.rightHandInteracting = true;
-					//Debug.Break();
+					rightHandInteracting = true;
 				}
 			}
 		}
+	}
 
-		
-
-		//player has grabbed wheel
-		if ( indexOfClosest >= 0 && mastInteraction.leftHandInteracting && Controller.LeftController.GetPress( Controller.Grip ) ) {
-			//print( "inside hold down left" );
-			//interacting with wheel
-			//needs to have constant hit detction, if hit index lower than closest then lower aim, else raaise it
-			//var lookPos = mastInteraction.leftHand.position - cannonCurrentlyAiming.handMarker.position;
-			//lookPos.x = 0;
-			//var rotation = Quaternion.LookRotation( lookPos );
-			//cannonCurrentlyAiming.handMarker.rotation = Quaternion.Slerp( transform.rotation, rotation, Time.deltaTime );
-
-			//cannonCurrentlyAiming.handMarker.LookAt( mastInteraction.leftHand );
-			//var rot = cannonCurrentlyAiming.handMarker.rotation;
-			if (Vector3.Distance( mastInteraction.leftHand.position, cannonCurrentlyAiming.aimingNodes[indexOfClosest].position )> maxReachToCannonWheel) {
-				StopInteracting();
-			}	
+	[ClientRpc]
+	private void RpcStartInteractingOnClient( GameObject cannon,GameObject player, bool isLeft, int iOfClosest ) {
+		if (player != gameObject && !isServer) {
+			return;
 		}
 
-		if ( indexOfClosest >= 0 && mastInteraction.rightHandInteracting && Controller.RightController.GetPress( Controller.Grip ) ) {
-			//interacting with wheel
-			//print( "inside hold down right" );
-			if ( Vector3.Distance( mastInteraction.rightHand.position, cannonCurrentlyAiming.aimingNodes[indexOfClosest].position ) > maxReachToCannonWheel ) {
-				StopInteracting();
-			}
+		foreach ( CannonAimNode node in cannon.GetComponentsInChildren<CannonAimNode>() ) {
+			node.gameObject.GetComponent<Renderer>().enabled = true;
 		}
 
+		cannonCurrentlyAiming = cannon.GetComponent<Cannon>();
+		indexOfClosest = iOfClosest;
 
-		//player let go
-		if (mastInteraction.leftHandInteracting && Controller.LeftController.GetPressUp( Controller.Grip ) ) {
-			//print( "inside up left" );
-			mastInteraction.leftHandInteracting = false;
-			StopInteracting();
-		}
-
-		if (mastInteraction.rightHandInteracting && Controller.RightController.GetPressUp( Controller.Grip ) ) {
-			//print( "inside up right" );
-			mastInteraction.rightHandInteracting = false;
-			StopInteracting();
+		if (isLeft) {
+			leftHandInteracting = true;
+		} else {
+			rightHandInteracting = true;
 		}
 	}
 
+	[ClientRpc]
+	private void RpcStopInteractingOnClient( GameObject cannon, GameObject player, bool isLeft ) {
+		if ( player != gameObject && !isServer ) {
+			return;
+		}
 
-	public void StopInteracting() {
+		foreach ( CannonAimNode node in cannon.GetComponentsInChildren<CannonAimNode>() ) {
+			node.gameObject.GetComponent<Renderer>().enabled = false;
+		}
+
+		cannonCurrentlyAiming = null;
+		indexOfClosest = -1;
+		if ( isLeft ) {
+			leftHandInteracting = false;
+		} else {
+			rightHandInteracting = false;
+		}
+	}
+
+	[Command]
+	public void CmdStopInteracting(bool isLeft) {
 		for ( int index = 0; index < cannonCurrentlyAiming.aimingNodes.Length; index++ ) {
 			Transform node = cannonCurrentlyAiming.aimingNodes[index];
-			node.GetComponent<Renderer>().enabled = false;
+			//node.GetComponent<Renderer>().enabled = false;
 			node.GetComponent<CannonAimNode>().player = null;
 		}
 
+		RpcStopInteractingOnClient( cannonCurrentlyAiming.gameObject, gameObject, isLeft );
 		cannonCurrentlyAiming.indexOfFirstGrabbed = -1;
 		cannonCurrentlyAiming = null;
 		indexOfClosest = -1;
--- FlipSwitch VR - Skeleton Crew/Assets/_Scripts/Skeleton Crew/Cannon.cs
+++ FlipSwitch VR - Skeleton Crew/Assets/_Scripts/Skeleton Crew/Cannon.cs
@@ -14,16 +14,16 @@ public class Cannon : NetworkBehaviour {
 	public Transform[] aimingNodes;
 	public Transform handMarker;
 
-	[SyncVar(hook = "OnFiringChange")]
+	[SyncVar( hook = "OnFiringChange" )]
 	private bool isFiring;
-	[SyncVar(hook = "OnReload")]
+	[SyncVar( hook = "OnReload" )]
 	private bool isReloaded = true;
 
-	void OnReload(bool n) {
+	void OnReload( bool n ) {
 		isReloaded = n;
 	}
 
-	void OnFiringChange(bool n) {
+	void OnFiringChange( bool n ) {
 		isFiring = n;
 	}
 
@@ -32,16 +32,16 @@ public class Cannon : NetworkBehaviour {
 	}
 
 	public void CreateCannonBall() {
-		if (!isReloaded) {
+		if ( !isReloaded ) {
 			return;
 		}
 
 		isFiring = true;
 		isReloaded = false;
-		GameObject bullet = Instantiate(projectile, spawnPos.position, Quaternion.identity);
-		Instantiate(smoke, spawnPos.position, Quaternion.identity);
+		GameObject bullet = Instantiate( projectile, spawnPos.position, Quaternion.identity );
+		Instantiate( smoke, spawnPos.position, Quaternion.identity );
 		bullet.GetComponent<Rigidbody>().velocity = spawnPos.forward * power;
-		Invoke("ReloadCannon", 3f);
+		Invoke( "ReloadCannon", 3f );
 		GetComponent<AudioSource>().clip = fireSound;
 		GetComponent<AudioSource>().Play();
 	}
@@ -54,40 +54,42 @@ public class Cannon : NetworkBehaviour {
 	public int indexOfFirstGrabbed = -1; //only being set on local player
 	int angleIncrement = 5;
 
-	
-	public void RotateBarrel(int indexOfNode) {
+
+	public void RotateBarrel( int indexOfNode ) {
 		if ( !isServer )
 			return;
 		//aiming is weird, -5 is the lowest, -45 is the highest. take in as positive and convert min and max to negative for best results
-		if (indexOfFirstGrabbed >= 0) {
-			int raiseSign = (indexOfNode > indexOfFirstGrabbed) ? 1 : -1; //if index is greater (closer to back of cannon) then you are raising the cannon
+		if ( indexOfFirstGrabbed >= 0 ) {
+			int raiseSign = ( indexOfNode > indexOfFirstGrabbed ) ? 1 : -1; //if index is greater (closer to back of cannon) then you are raising the cannon
 
-			float barrelRotation = cannonBarrel.transform.rotation.x;
-			float targetAngel = Mathf.Abs( barrelRotation + ( raiseSign * angleIncrement ) );
-			print("current index " + indexOfFirstGrabbed + " index that called " + indexOfNode + " "+ barrelRotation + " plus " + ( raiseSign * angleIncrement ) + " becomes target of "+ targetAngel);
+			float barrelRotation = cannonBarrel.localEulerAngles.x;
+			float targetAngle = Mathf.Abs( barrelRotation + ( raiseSign * angleIncrement ) );
+			print( "current index " + indexOfFirstGrabbed + " index that called " + indexOfNode + " " + barrelRotation + " plus " + ( raiseSign * angleIncrement ) + " becomes target of " + targetAngle );
 
-			if (targetAngel <= maxAngle && targetAngel >= minAngle) {
-				//perform rotation
-				print(targetAngel + " is within range, rotate barrel");
-				cannonBarrel.localRotation = Quaternion.Euler(targetAngel, 0, 0);
-			} else {
-				print( targetAngel + " is not within range, do not rotate barrel" );
-			}
+			//if (targetAngel <= maxAngle && targetAngel >= minAngle) {
+			//perform rotation
+			print( targetAngle + " is within range, rotate barrel" );
+			cannonBarrel.localEulerAngles = new Vector3( targetAngle, 0, 0 );
+			print( "AFTER " + barrelRotation + " is old,  " + cannonBarrel.localRotation + " is new, target was " + targetAngle );
+
+			//} else {
+			//	print( targetAngel + " is not within range, do not rotate barrel" );
+			//}
 
 			//RpcRotateBarrel( cannonBarrel.localRotation );
 		}
 	}
 
-	public void RpcRotateBarrel(Quaternion newRot) {
-		if (isServer) {
+	public void RpcRotateBarrel( Quaternion newRot ) {
+		if ( isServer ) {
 			return;
 		}
 		cannonBarrel.localRotation = newRot;
 	}
 
 	private void OnDrawGizmos() {
-		if (spawnPos) {
-			Gizmos.DrawLine(spawnPos.transform.position, spawnPos.transform.position + spawnPos.transform.forward * 2);
+		if ( spawnPos ) {
+			Gizmos.DrawLine( spawnPos.transform.position, spawnPos.transform.position + spawnPos.transform.forward * 2 );
 		}
 	}
 }
