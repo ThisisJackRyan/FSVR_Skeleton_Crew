e052a02c7a4d21e853c0862363997b2c908a18d3
FLVR1001\fsvr1001 <plex>
2018-08-15 14:29:32 -0500

    multipattern for damaged objects

--- FlipSwitch VR - Skeleton Crew/Assets/_Scripts/Interaction/RepairTrigger.cs
+++ FlipSwitch VR - Skeleton Crew/Assets/_Scripts/Interaction/RepairTrigger.cs
@@ -4,43 +4,59 @@ using UnityEngine;
 
 public class RepairTrigger : MonoBehaviour {
 
-	public RepairPattern repairPattern;
+	public RepairPattern[] repairPatterns;
+	RepairPattern repairPattern;
+	Transform activator;
 
 	float timer = 0;
 	bool active = false;
 
 	private void OnTriggerStay( Collider other ) {
-		if (other.gameObject.GetComponentInParent<MastInteraction>() && active) {
+		if (other.transform.root == activator && active) {
 			timer += Time.deltaTime;
 
 			if (timer >= 1) {
-				repairPattern.gameObject.SetActive( true );
-				repairPattern.Init();
+				repairPattern.gameObject.SetActive( true );//
+				repairPattern.Init();//
 				active = false;
 
 				GetComponent<Renderer>().enabled = false;
 				transform.position = new Vector3( transform.position.x, other.transform.root.GetComponentInChildren<HipMarker>().transform.position.y, transform.position.z );
-
 			}
 		}
 	}
 
 	private void OnTriggerEnter( Collider other ) {
 		if ( other.gameObject.GetComponentInParent<MastInteraction>() ) {
-			if (repairPattern.gameObject.activeInHierarchy) {
+			if (repairPattern != null && repairPattern.gameObject.activeInHierarchy) {//
 				return;
 			}
 
 			timer = 0;
 			active = true;
 			GetComponent<Renderer>().enabled = true;
-			repairPattern.gameObject.SetActive( false );
 
+			int rng = Random.Range(0, repairPatterns.Length);
+			repairPattern = repairPatterns[rng];
+
+			foreach (var pat in repairPatterns) {
+				pat.gameObject.SetActive(false);
+			}
+
+			activator = other.transform.root;
+
+			//repairPattern.gameObject.SetActive( false );//
 		}
 	}
 
 	private void OnTriggerExit( Collider other ) {
+		if (other.transform.root != activator) {
+			return;
+		}
+
 		active = false;
+		repairPattern = null;
+		activator = null;
 	}
 
 }
