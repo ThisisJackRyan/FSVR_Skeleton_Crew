2b23f794c102d4b5b0d291665bebc3bca9b9b484
nboehning <nboehning@gmail.com>
2018-08-22 04:10:45 -0500

    (All need testing) Added in script to randomly generate a point for cannons to hit. Modified enemy behaviors: crewmen and bosses unparent the ship after boarding player ship. bosses now teleport 3 seconds after arriving. Boarding ship now destroys itself after 4 seconds.

---
+++ FlipSwitch VR - Skeleton Crew/Assets/ImpactReticuleSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ImpactReticuleSpawner : NetworkBehaviour {
+
+    public GameObject deckMesh;
+    public GameObject reticlePrefab;
+    public float minHeight, maxHeight;
+
+	private Vector3 GeneratePoint() {
+        Vector3 retVect = Vector3.zero;
+
+        Bounds deckBounds = deckMesh.GetComponent<Mesh>().bounds;
+
+        float x = Random.Range(deckBounds.min.x + 1, deckBounds.max.x - 1);
+        float z = Random.Range(deckBounds.min.z + 1, deckBounds.max.z - 1);
+        retVect.Set(x, 0, z);
+        return retVect;
+    }
+
+    public void SpawnReticle() {
+        if (!isServer) {
+            return;
+        }
+
+        GameObject reticle = Instantiate(reticlePrefab, GeneratePoint(), Quaternion.identity);
+        NetworkServer.Spawn(reticle);
+    }
+}
