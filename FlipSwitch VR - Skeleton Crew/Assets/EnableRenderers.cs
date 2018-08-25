8eab2e05dccdd580d7dbbbc30a8cbee212c5991b
FLVR1001\fsvr1001 <plex>
2018-08-25 13:24:03 -0500

    respawn work

---
+++ FlipSwitch VR - Skeleton Crew/Assets/EnableRenderers.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableRenderers : MonoBehaviour {
+
+	// Use this for initialization
+	void LateUpdate () {
+	
+		foreach(var r in GetComponentsInChildren<Renderer>() ) {
+			//print(r.name);
+			r.enabled = true;
+		}
+	}
+
+	//private void LateUpdate() {
+
+	//}
+
+
+
+}
