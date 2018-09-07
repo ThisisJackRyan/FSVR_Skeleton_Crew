0330b44a288ce2fc620a443528f201fb82447380
Nathan <nboehning@gmail.com>
2018-09-07 15:56:51 -0500

    Missed some changes last night that weren't saved during commit.

--- FlipSwitch VR - Skeleton Crew/Assets/Add-Ons/Behavior Designer/Third Party/Third Person Controller/Tasks/Use.cs
+++ FlipSwitch VR - Skeleton Crew/Assets/Add-Ons/Behavior Designer/Third Party/Third Person Controller/Tasks/Use.cs
@@ -41,7 +41,6 @@ namespace BehaviorDesigner.Runtime.Tasks.ThirdPersonController
                 return itemHandler.TryUseItem(useItemType == UseItemType.Primary ? typeof(PrimaryItemType) : 
                                                 (useItemType == UseItemType.DualWield ? typeof(DualWieldItemType) : typeof(SecondaryItemType))) ? TaskStatus.Success : TaskStatus.Failure;
             }
-            Debug.Log("calling use");
             return itemHandler.TryUseItem(itemType.Value.GetType()) ? TaskStatus.Success : TaskStatus.Failure;
         }
 
