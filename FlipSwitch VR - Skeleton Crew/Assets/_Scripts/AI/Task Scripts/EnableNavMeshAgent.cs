33fae7f809be00d6272b66bc7294378334299726
Nathan <nboehning@gmail.com>
2018-09-14 03:48:44 -0500

    Fixed enemy movement animations not being synced on Guard (still need to apply to crewmen + boss variants). Created ratkin TPC.

--- FlipSwitch VR - Skeleton Crew/Assets/_Scripts/AI/Task Scripts/EnableNavMeshAgent.cs
+++ FlipSwitch VR - Skeleton Crew/Assets/_Scripts/AI/Task Scripts/EnableNavMeshAgent.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine.AI;
 using BehaviorDesigner.Runtime;
 using BehaviorDesigner.Runtime.Tasks;
+using Opsive.ThirdPersonController.Wrappers;
 
 public class EnableNavMeshAgent : Action {
 
@@ -10,5 +11,6 @@ public class EnableNavMeshAgent : Action {
 	public override void OnStart () {
         GetComponent<NavMeshAgent>().enabled = true;
         GetComponent<NavMeshAgent>().autoTraverseOffMeshLink = true;
+        GetComponent<NavMeshAgentBridge>().enabled = true;
 	}
 }
