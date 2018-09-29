89839db6d0000ce1437112b0dc3899caa86fd361
FSVR6001\A1P1 <a1p1@fsvr.com>
2018-09-29 16:04:29 -0500

    ratkin spawning animated

---
+++ FlipSwitch VR - Skeleton Crew/Assets/LobbyAudioPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyAudioPlayer : MonoBehaviour {
+
+    public AudioClip moveClip;
+
+    public void PlayNewClip() {
+        GetComponent<AudioSource>().clip = moveClip;
+        GetComponent<AudioSource>().loop = false;
+        GetComponent<AudioSource>().Play();
+        Destroy(gameObject, moveClip.length + 0.5f);
+    }
+
+}
