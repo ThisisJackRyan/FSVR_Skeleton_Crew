5a860e249cda6e0f90c5df956c2eff0d99effd74
FSVR-1001 <>
2018-09-29 18:12:20 -0500

    updated plyer

--- FlipSwitch VR - Skeleton Crew/Assets/RotationTester.cs
+++ FlipSwitch VR - Skeleton Crew/Assets/RotationTester.cs
@@ -5,18 +5,14 @@ using UnityEngine;
 public class RotationTester : MonoBehaviour {
 
     public Transform toFollow;
-    Quaternion holdingRot;
+    //Quaternion holdingRot;
 	// Update is called once per frame
 	void Update () {
-        if (toFollow) {
+		if (toFollow) {
 
-            Vector3 facing = toFollow.position - transform.position;
-            facing.x = 0;
-            transform.rotation = (holdingRot * Quaternion.LookRotation(facing, new Vector3(0, -1, 0)) );
-
-        } else {
-            print("updating holding rot");
-            holdingRot = transform.rotation;
-        }
+			Vector3 facing = toFollow.position - transform.position;
+			facing.x = 0;
+			transform.rotation = Quaternion.LookRotation(facing, new Vector3(0, -1, 0) );
+		}
 	}
 }
