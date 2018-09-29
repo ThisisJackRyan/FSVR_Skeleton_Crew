b70712901d8f84628fa35dfdb2f1b9fff8b08346
FSVR6001\A1P1 <a1p1@fsvr.com>
2018-09-29 17:42:28 -0500

    began dissonance, test on rotation stuff

--- FlipSwitch VR - Skeleton Crew/Assets/RotationTester.cs
+++ FlipSwitch VR - Skeleton Crew/Assets/RotationTester.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class RotationTester : MonoBehaviour {
 
     public Transform toFollow;
+    Quaternion holdingRot;
 	// Update is called once per frame
 	void Update () {
         if (toFollow) {
 
-        Vector3 facing = toFollow.position - transform.position;
-        facing.x = 0;
-        transform.rotation = Quaternion.LookRotation(facing, new Vector3(0, -1, 0));
+            Vector3 facing = toFollow.position - transform.position;
+            facing.x = 0;
+            transform.rotation = (holdingRot * Quaternion.LookRotation(facing, new Vector3(0, -1, 0)) );
 
+        } else {
+            print("updating holding rot");
+            holdingRot = transform.rotation;
         }
 	}
 }
