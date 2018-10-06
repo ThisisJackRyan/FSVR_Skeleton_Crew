c17a35e76d2c7c6b6700b205167f961716b3fc67
FSVR6001\A1P1 <a1p1@fsvr.com>
2018-10-06 14:26:50 -0500

    Anotha one

---
+++ FlipSwitch VR - Skeleton Crew/Assets/FSVR_AssignController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FSVR_AssignController : MonoBehaviour {
+
+    SteamVR_Controller.Device device;
+    public Text canvasText;
+
+	void Start () {
+        device = Controller.GetById((int)GetComponent<SteamVR_TrackedObject>().index);
+        //canvasText = GetComponentInChildren<Text>();
+    }
+	
+	
+	void Update () {
+        if (device != null){
+            //print(device + " id is " + device.index);
+
+            if (device.GetPressDown(Controller.Trigger)) {
+                //print("hit trigger on device i:" + device.index);
+                canvasText.transform.parent.gameObject.SetActive(true);
+                canvasText.text = Controller.InitControllers(device.index);
+
+                if (Controller.initialized) {
+                    FindObjectOfType<ConnectWithPress>().canInput = true;
+                }
+            }
+
+
+        }
+	}
+}
