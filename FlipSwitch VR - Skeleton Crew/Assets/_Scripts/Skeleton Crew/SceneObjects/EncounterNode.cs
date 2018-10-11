405a50825fff6b50a074c682951332a251a440ee
devteam <@washington>
2018-10-11 17:35:14 -0500

    Reverted encounter node. Added logic that was in encounter node to generic spawning in pathfollower.

--- FlipSwitch VR - Skeleton Crew/Assets/_Scripts/Skeleton Crew/SceneObjects/EncounterNode.cs
+++ FlipSwitch VR - Skeleton Crew/Assets/_Scripts/Skeleton Crew/SceneObjects/EncounterNode.cs
@@ -4,28 +4,15 @@ using UnityEngine.Networking;
 using Random = UnityEngine.Random;
 
 public class EncounterNode : MonoBehaviour {
-	public GameObject rangedPrefabToSpawn;
-    public GameObject meleePrefabToSpawn;
+	public GameObject prefabToSpawn;
 	[Tooltip("OPTIONAL; if not assigned will use this transform.")]
 	public Transform spawnPos;
 
-	internal void SpawnEncounter() {
+    internal void SpawnEncounter() {
         if (spawnPos == null) {
             spawnPos = transform;
         }
-        if (NumberOfPlayerHolder.instance.numberOfPlayers <= 2) {
-            if (VariableHolder.instance.numRangedUnits > 2) {
-                int rand = Random.Range(0, 100);
-                if (rand <= 49) {
-                    GetComponentInParent<EncounterSpawner>().SpawnEncounter(rangedPrefabToSpawn, spawnPos);
-                } else {
-                    GetComponentInParent<EncounterSpawner>().SpawnEncounter(meleePrefabToSpawn, spawnPos);
-                }
-            } else {
-                GetComponentInParent<EncounterSpawner>().SpawnEncounter(rangedPrefabToSpawn, spawnPos);
-            }
-        } else {
-            GetComponentInParent<EncounterSpawner>().SpawnEncounter(rangedPrefabToSpawn, spawnPos);
-        }
-	}
+
+        GetComponentInParent<EncounterSpawner>().SpawnEncounter(prefabToSpawn, spawnPos);
+    }
 }
