4cf4c866413bd3acb65d60fe97bd9a007d03a75f
FSVR6001\A1P1 <a1p1@fsvr.com>
2018-10-23 14:05:26 -0500

    captain shockwave effect integrated

--- FlipSwitch VR - Skeleton Crew/Assets/Art/Shockwave_FX/Scripts/Shockwave.cs
+++ FlipSwitch VR - Skeleton Crew/Assets/Art/Shockwave_FX/Scripts/Shockwave.cs
@@ -3,84 +3,79 @@ using System.Collections;
 
 public class Shockwave : MonoBehaviour {
 
-public GameObject ShockwaveFX;
-public Light ShockwaveLight;
-public float lightRangeSizeStart;
-public float lightRangeSizeEnd;
-public float lightIntensity = 8;
+	public GameObject ShockwaveFX;
+	public Light ShockwaveLight;
+	public float lightRangeSizeStart;
+	public float lightRangeSizeEnd;
+	public float lightIntensity = 8;
 
-private float t = 0.0f;
-private float fadeEnd = 0;
-private float fadeTime = 2;
-private float pauseTime = 0;
-private float fadeRange = 3.5f;
-private bool activeShockwave = false;
+	private float t = 0.0f;
+	private float fadeEnd = 0;
+	private float fadeTime = 2;
+	private float pauseTime = 0;
+	private float fadeRange = 3.5f;
+	private bool activeShockwave = false;
 
+	void Start() {
 
+		ShockwaveFX.SetActive(false);
+		fadeRange = lightRangeSizeStart;
 
+		StartCoroutine("launchShockwave");
 
-void Start (){
-
-    ShockwaveFX.SetActive(false);
-    fadeRange = lightRangeSizeStart;
-
-}
-
- 
-void Update (){
-
-    if (Input.GetButtonDown("Fire1"))
-    {
-
-        if (activeShockwave == false)
-        {
-			 StartCoroutine("launchShockwave");
-        }
-
-    }
-   
-}
+	}
 
 
-IEnumerator FadeLight (){
-   
-     while (t < fadeTime) 
-     {
+	//	void Update (){
 
-         if (pauseTime == 0)
-         {
-             t += Time.deltaTime;
-         }
-          
-         if (fadeRange < lightRangeSizeEnd)
-         {
-             fadeRange = fadeRange + 0.5f;
-         }
+	//    if (Input.GetButtonDown("Fire1"))
+	//    {
 
-         ShockwaveLight.intensity = Mathf.Lerp(lightIntensity, fadeEnd, t / fadeTime);
-         ShockwaveLight.range = fadeRange;
-         yield return 0;
+	//        if (activeShockwave == false)
+	//        {
+	//        }
 
-     }                      
-            
-t = 0;
-    
-}
+	//    }
+
+	//}
 
 
-IEnumerator launchShockwave (){
+	IEnumerator FadeLight() {
 
-    ShockwaveFX.SetActive(true);
-	StartCoroutine("FadeLight");
-  
+		while (t < fadeTime) {
 
-    activeShockwave = true;
+			if (pauseTime == 0) {
+				t += Time.deltaTime;
+			}
 
-	yield return new  WaitForSeconds (4);  //  Wait for the shockwave to finish
-   
-    activeShockwave = false;
-    ShockwaveFX.SetActive(false);
+			if (fadeRange < lightRangeSizeEnd) {
+				fadeRange = fadeRange + 0.5f;
+			}
 
-}
+			ShockwaveLight.intensity = Mathf.Lerp(lightIntensity, fadeEnd, t / fadeTime);
+			ShockwaveLight.range = fadeRange;
+			yield return 0;
+
+		}
+
+		t = 0;
+
+	}
+
+
+	IEnumerator launchShockwave() {
+
+		ShockwaveFX.SetActive(true);
+		StartCoroutine("FadeLight");
+
+
+		activeShockwave = true;
+
+		yield return new WaitForSeconds(4);  //  Wait for the shockwave to finish
+
+		activeShockwave = false;
+		ShockwaveFX.SetActive(false);
+
+	}
 
 }
